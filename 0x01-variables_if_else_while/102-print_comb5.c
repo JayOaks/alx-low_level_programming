a019968b41520ff734ba1505daa56d6a4a4efdc6
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-08 22:42:25 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int i, j;
 
 for (i = 0; i <= 99; i++)
 {
-for (j = I + 1; j <= 99; j++)
+for (j = i + 1; j <= 99; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
@@ -22,7 +22,7 @@ putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
 
-if (i != 99 || j != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
