289a8bc0172de201e3a48fa960775f1bf2c7940b
Jeroldine <oakleyjeroldine@gmail.com>
2023-09-08 20:17:48 +0000

    Create files for tasks 7 and 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Hexadecimals
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 0; i < 16; i++)
+{
+putchar(i < 0 ? i + '0' : i + 'a' - 10);
+}
+putchar('\n');
+return (0);
+}
