04a99c0eee50a6a4ad7f389de4b88a9217c6af9c
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-12 23:45:44 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,17 @@
 
 /**
 * _islower - checks if a character is lowercase
+* return - 1 = character is alphabetic, 0 = character is not alphabetic
 **/
 
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
 {
-return 1;
+return (1);
 }
 else
 {
-return 0;
+return (0);
 }
 }
