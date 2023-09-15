8d06d299912c0a5a302688e79434d4ea6bb95273
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-15 01:46:53 +0000

    Update 1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,21 +1,16 @@
 #include "main.h"
-#include <stdio.h>
 
-/*
-* _isdigit - checks if input is a digit between 0 and 9
-* @c : the input to be assessed
-* Return: 1 for digits between 0 and 9, 
-*         0 for anything not a digit between 0 and 9
-*/
+/**
+ * _isdigit - Entry point
+ * Description: Checks for a digit between 0 & 9
+ * @c: Integer
+ * Return: 1 if true, 0 if false
+ */
 
 int _isdigit(int c)
 {
-if ((c >= '0') && (c <= '9'))
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
