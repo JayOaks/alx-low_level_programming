94bebcc1b8d4947574f31ab6826714973d455693
Jeroldine <oakleyjeroldine@gmail.com>
2023-09-23 00:13:20 +0000

    Update main.h & 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,17 +8,21 @@
  * Return: returns pointer to @dest
  */
 
+
 char *_strcat(char *dest, char *src)
 {
+	char *p = dest;
 
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	while (*p != '\0')
+	{
+		p++;
+	}
+	while (*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	*p = '\0';
 	return (dest);
 }
