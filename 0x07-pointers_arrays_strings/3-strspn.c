65d6fde0be0f9a98f7ffaccddba220a9b0b9dfd5
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-27 01:21:55 +0000

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
 /**
- * _strspn - a function that gets the length of a prefix substring
- * @s: the string to be assessed
- * @accept: the accepted bytes 
- * Return: the number of bytes
+ * _strspn - Entry point
+ * Description: Gets the length of a prefix substring
+ * @s: Character
+ * @accept: Character
+ * Return: char
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int z = 0, x, y;
+	unsigned int len = 0;
+	int i, j;
 
-	for (x = 0; s[x] != '\0'; x++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[x] != 32)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			for (y = 0; accept[y] != '\0'; y++)
+			if (s[i] == accept[j])
 			{
-				if (s[x] == accept[y])
-					z++;
+				len++;
+				break;
 			}
 		}
-		else
-			return (z);
+		if (accept[j] == '\0')
+		{
+			return (len);
+		}
 	}
-		return (z);
-
+	return (len);
 }
