da85f16f2fca13b973c7f1e7603ebc5aabf7262e
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-27 01:38:57 +0000

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,29 +10,24 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-    if (haystack == NULL || needle == NULL)
-    {
-        return (NULL);
-    }
-    
-    while (*haystack != '\0')
-    {
-        char *h = haystack;
-        char *n = needle;
-
+    if (*needle == '\0')
+	{
+		return (haystack);
+	}
+    while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
         while (*n != '\0' && *h == *n)
-        {
-            h++;
-            n++;
-        }
-
-        if (*n == '\0')
-        {
-            return (haystack);
-        }
-        
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
         haystack++;
-    }
-
-return (NULL);
+	}
+    return ('\0');
 }
