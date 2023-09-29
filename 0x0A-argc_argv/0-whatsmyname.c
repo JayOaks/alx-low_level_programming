0be49957e278877cec636b32d2f67f037a2b0990
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-29 21:58:18 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,8 +9,10 @@
 * Return: 0 on successful execution.
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char **argv)
 {
+(void)argc;
+
 printf("%s\n", argv[0]);
 return (0);
 }
