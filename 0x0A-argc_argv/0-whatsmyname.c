48b9d91fc88ecd6ff115beba54649e1b0ef067b8
Jeroldine Akuye Oakley <68546366+JayOaks@users.noreply.github.com>
2023-09-29 21:55:11 +0000

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,15 +3,15 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point of the program
- * Description: Prints the program name (including the path) followed by a new line.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the command-line arguments.
- * Return: 0 on successful execution.
- */
+* main - Entry point of the program
+* Description: Prints the program name (including the path) followed by a new line.
+* @argc: The number of command-line arguments.
+* @argv: An array of strings containing the command-line arguments.
+* Return: 0 on successful execution.
+*/
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
-	return (0);
+printf("%s\n", argv[0]);
+return (0);
 }
