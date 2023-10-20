c968196a60e2eaba0e7e3915e7f77ba31338e727
Jeroldine <oakleyjeroldine@gmail.com>
2023-10-20 05:57:45 +0000

    Update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 
-void preMain(void);
+void ahead(void) __attribute__ ((constructor));
 
-int main()
-{
-preMain();
-
-// The main function code goes here
-// ...
-
-return 0;
-}
-
-void preMain(void)
+void ahead(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
