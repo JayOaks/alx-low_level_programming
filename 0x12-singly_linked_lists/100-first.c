de0dcc900bb5657eebf8a7293cdc3b443342401c
Jeroldine <oakleyjeroldine@gmail.com>
2023-10-20 05:47:53 +0000

    Update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void preMain(void);
+
+void preMain(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
+
+int main()
+{
+preMain();
+
+// The main function code goes here
+// ...
+
+return 0;
+}
