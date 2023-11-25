688dec3bd40197f0a2141790b609eb1c4744cb94
Jeroldine <oakleyjeroldine@gmail.com>
2023-11-25 01:03:02 +0000

    Update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,58 +3,64 @@
 #include <stdio.h>
 
 /**
- * _atoi - Converts a character array to an integer
+ * _atoll - Converts a character array to a long long integer
  * @s: character array to convert
- * Return: returns integer from array, returns 0 if none found
+ * Return: returns long long integer from array, returns 0 if none found
  */
 
-int _atoi(char *s)
+long long _atoll(char *s)
 {
-int retval, i, neg;
-i = 0;
-retval = 0;
-neg = -1;
-while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
-{
+long long retval = 0; 
+int i = 0, neg = 1;
+
 if (s[i] == '-')
-neg *= -1;
+{
+neg = -1;
 i++;
 }
 while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
-retval = (retval * 10) - (s[i++] - '0');
+{
+retval = (retval * 10) + (s[i] - '0');
+i++;
+}
 return (retval *neg);
 }
 
 /**
  * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
+ * @argc: number of arguments
+ * @argv: argument vector
  * Return: int
  */
 
 int main(int argc, char *argv[])
 {
-int a, b, i, j;
-unsigned long mul;
+long long a, b;
+unsigned long long mul;
 if (argc != 3)
 {
 printf("Error\n");
 exit(98);
 }
-for (i = 1; i < argc; i++)
+for (int i = 1; i < argc; i++)
 {
-for (j = 0; argv[i][j] != '\0'; j++)
+for (int j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][j] > 57 || argv[i][j] < 48)
+if (argv[i][j] > '9' || argv[i][j] < '0')
 {
 printf("Error\n");
 exit(98);
 }
 }
 }
-a = _atoi(argv[1]);
-b = _atoi(argv[2]);
+a = _atoll(argv[1]);
+b = _atoll(argv[2]);
+if (a <= 0 || b <= 0)
+{
+printf("Error\n");
+exit(98);
+}
 mul = a *b;
-printf("%lu\n", mul);
+printf("%llu\n", mul);
 return (0);
 }
