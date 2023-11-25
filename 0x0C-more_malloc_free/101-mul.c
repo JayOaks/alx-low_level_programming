6154877f3e2bc59e6cefc9d73429d6ca97c2b5e8
Jeroldine <oakleyjeroldine@gmail.com>
2023-11-25 01:43:20 +0000

    Update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,13 +10,19 @@
 
 int _atoi(char *s)
 {
-int retval, i;
+int retval, i, neg;
+i = 0;
 retval = 0;
-for (i = 0; s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'); i++)
+neg = -1;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 {
-retval = (retval * 10) + (s[i] - '0');
-{
-return (retval);
+if (s[i] == '-')
+neg *= -1;
+i++;
+}
+while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+retval = (retval * 10) - (s[i++] - '0');
+return (retval *neg);
 }
 
 /**
@@ -30,29 +36,25 @@ int main(int argc, char *argv[])
 {
 int a, b, i, j;
 unsigned long mul;
-
 if (argc != 3)
 {
 printf("Error\n");
 exit(98);
 }
-
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
 {
-if (argv[i][j] < '0' || argv[i][j] > '9')
+if (argv[i][j] > 57 || argv[i][j] < 48)
 {
 printf("Error\n");
 exit(98);
 }
 }
 }
-
 a = _atoi(argv[1]);
 b = _atoi(argv[2]);
-mul = (unsigned long)a * (unsigned long)b;
+mul = a *b;
 printf("%lu\n", mul);
-
 return (0);
 }
