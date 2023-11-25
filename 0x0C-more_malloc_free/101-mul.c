26b136aef2319ea7ca0c229985b43d5d08930a2a
Jeroldine <oakleyjeroldine@gmail.com>
2023-11-25 01:25:46 +0000

    Update

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,59 +3,58 @@
 #include <stdio.h>
 
 /**
- * my_atoi - Converts a string to an integer, assuming it's a positive number
- * @str: The string to convert
- * Return: The converted integer, 
- * or -1 if the string is not a valid positive number
+ * _atoi - Converts a character array to an integer
+ * @s: character array to convert
+ * Return: returns integer from array, returns 0 if none found
  */
+
 int _atoi(char *s)
 {
-int result = 0;
-
-if (s[0] == '\0')
+int retval, i, neg;
+i = 0;
+retval = 0;
+neg = -1;
+while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 {
-return (-1);
+if (s[i] == '-')
+neg *= -1;
+i++;
 }
-
-for (int i = 0; s[i] != '\0'; i++)
-{
-if (s[i] < '0' || s[i] > '9')
-{
-return (-1);
-}
-result = result * 10 + (s[i] - '0');
-}
-return (result);
+while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+retval = (retval * 10) - (s[i++] - '0');
+return (retval *neg);
 }
 
 /**
- * main - Multiplies two positive numbers
- * @argc: Argument count
- * @argv: Argument vector
- * Return: 0 on success, 98 on error
+ * main - multiplies two positive numbers
+ * @argc: n arguments
+ * @argv: args
+ * Return: int
  */
+
 int main(int argc, char *argv[])
 {
-int num1, num2;
-long mul;
-
+int a, b, i, j;
+unsigned long mul;
 if (argc != 3)
 {
 printf("Error\n");
 exit(98);
 }
-
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-
-if (num1 <= 0 || num2 <= 0)
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if (argv[i][j] > 57 || argv[i][j] < 48)
 {
 printf("Error\n");
 exit(98);
 }
-
-mul = (long)num1 *num2;
-printf("%ld\n", mul);
-
+}
+}
+a = _atoi(argv[1]);
+b = _atoi(argv[2]);
+mul = a *b;
+printf("%lu\n", mul);
 return (0);
 }
