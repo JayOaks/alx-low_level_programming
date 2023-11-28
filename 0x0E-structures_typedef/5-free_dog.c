39b00440135981c01944fd733a165c08d3363b32
Jeroldine <oakleyjeroldine@gmail.com>
2023-11-28 00:09:52 +0000

    Learning about Structures in C

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the dogs by freeing the memory
+ * allocated to their information
+ * @d: is the pointer to the dog/dog's information to freed
+ *
+ * Return: void
+**/
+
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
