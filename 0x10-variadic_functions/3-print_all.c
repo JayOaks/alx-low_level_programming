435642fa4b8de533891b9d3247ba9f5615f9402f
Jeroldine <oakleyjeroldine@gmail.com>
2023-12-01 03:24:53 +0000

    Trial

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,41 +8,41 @@
 
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0;
-    char *str;
-    char format_char;
+va_list args;
+int i = 0;
+char *str;
+char format_char;
 
-    va_start(args, format);
+va_start(args, format);
 
-    while (format && format[i])
-    {
-        format_char = format[i];
-        if (format_char == 'c' || format_char == 'i' || format_char == 'f' || format_char == 's')
-        {
-            printf("%s", (i > 0) ? ", " : "");
-            switch (format_char)
-            {
-                case 'c':
-                    printf("%c", va_arg(args, int));
-                    break;
-                case 'i':
-                    printf("%d", va_arg(args, int));
-                    break;
-                case 'f':
-                    printf("%f", va_arg(args, double));
-                    break;
-                case 's':
-                    str = va_arg(args, char *);
-                    if (str == NULL)
-                        str = "(nil)";
-                    printf("%s", str);
-                    break;
-            }
-        }
-        i++;
-    }
+while (format && format[i])
+{
+format_char = format[i];
+if (format_char == 'c' || format_char == 'i' ||
+format_char == 'f' || format_char == 's')
+{
+printf("%s", (i > 0) ? ", " : "");
+switch (format_char)
+{
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str) str = "(nil)";
+printf("%s", str);
+break;
+}
+}
+i++;
+}
 
-    va_end(args);
-    printf("\n");
+va_end(args);
+printf("\n");
 }
