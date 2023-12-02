e3c24abe55c7b780555a3e8d1948441459a37bf9
Jeroldine Oakley <oakleyjeroldine@gmail.com>
2023-12-02 04:18:51 +0000

    Update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,30 +10,30 @@
 
 int main(int argc, char *argv[])
 {
-    int num_bytes, i;
-    unsigned char *main_ptr;
+int num_bytes, i;
+unsigned char *main_ptr;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
 
-    num_bytes = atoi(argv[1]);
-    if (num_bytes < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
+num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
 
-    main_ptr = (unsigned char *)main;
-    for (i = 0; i < num_bytes; i++)
-    {
-        printf("%02x", main_ptr[i]);
-        if (i < num_bytes - 1)
-            printf(" ");
-    }
-    printf("\n");
+main_ptr = (unsigned char *)main;
+for (i = 0; i < num_bytes; i++)
+{
+printf("%02x", main_ptr[i]);
+if (i < num_bytes - 1)
+printf(" ");
+}
+printf("\n");
 
-    return (0);
+return (0);
 }
