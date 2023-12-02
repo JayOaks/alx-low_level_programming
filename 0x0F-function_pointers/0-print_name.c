0ccf8a755987c2a3be758dc1154a155ec98c18de
Jeroldine Oakley <oakleyjeroldine@gmail.com>
2023-12-02 04:26:21 +0000

    Update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,15 @@
  * print_name - prints a name
  * @name: name given as parameter
  * @f: function pointer
+ * 
  * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 if (!name || !f)
+{
 return;
+}
 f(name);
 }
