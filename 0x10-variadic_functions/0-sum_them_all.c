6cdfcbf8b524e74cd24ad74e53630d733fe2d65e
Jeroldine Oakley <oakleyjeroldine@gmail.com>
2023-12-02 03:07:52 +0000

    Update

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,14 +15,15 @@ va_list args;
 va_start(args, n);
 
 if (n == 0)
+{
 return (0);
-
+}
 
 for (i = 0; i < n; i++)
 {
 sum += va_arg(args, int);
 }
-va_end(args);
 
+va_end(args);
 return (sum);
 }
