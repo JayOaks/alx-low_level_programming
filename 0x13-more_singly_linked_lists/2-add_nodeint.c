52bc80636948ca30e30e476789bec71b0faddae5
Jeroldine Oakley <oakleyjeroldine@gmail.com>
2023-12-13 02:27:40 +0000

    Learning More About Singly Linked Lists in C

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,20 +4,20 @@
  * add_nodeint - Adds a new node at the beginning of a listint_t list
  * @head: Pointer to the pointer to the head of the list
  * @n: The integer for the new node to contain
- * 
+ *
  * Return: The address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *nnode;
+listint_t *nnode;
 
-    nnode = malloc(sizeof(listint_t));
-    if (nnode == NULL)
-        return (NULL);
+nnode = malloc(sizeof(listint_t));
+if (nnode == NULL)
+return (NULL);
 
-    nnode->n = n;
-    nnode->next = *head;
-    *head = nnode;
+nnode->n = n;
+nnode->next = *head;
+*head = nnode;
 
-    return (nnode);
+return (nnode);
 }
