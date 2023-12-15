2c1f708977f91805ccb77550198dc79e82ac7000
Jeroldine Oakley <oakleyjeroldine@gmail.com>
2023-12-15 05:33:25 +0000

    Update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,18 +10,32 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int i;
 unsigned int num = 0;
+unsigned int i = 0;
 
-if (b == NULL)
-return (0);
-
-for (i = 0; b[i] != '\0'; ++i)
+if (!b)
 {
-if (b[i] != '0' && b[i] != '1')
 return (0);
-num = 2 * num + (b[i] - '0');
 }
 
+while (b[i] != '\0')
+{
+if (b[i] != '0' && b[i] != '1')
+{
+return (0);
+}
+i++;
+}
+
+i = 0;
+while (b[i] != '\0')
+{
+num <<= 1;
+if (b[i] == '1')
+{
+num += 1;
+}
+i++;
+}
 return (num);
 }
